9a4e6d1c64c1b8a6c7a94bf55f856af4bd83772a
Aishwant Ghimire <aghimire@go.olemiss.edu>
2019-02-09 18:40:17 -0600

    To set up a layout for the Website

---
+++ src/frontend/src/components/layout/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Header extends Component {
+    render() {
+        return (
+            <nav>
+                Here goes our Header
+            </nav>
+        )
+    }
+}
+
+export default Header
