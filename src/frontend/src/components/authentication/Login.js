3940f53edff3137343c1a6df17f342475307354b
Aishwant Ghimire <aghimire@go.olemiss.edu>
2019-02-11 15:44:41 -0600

    Added form

--- src/frontend/src/components/authentication/Login.js
+++ src/frontend/src/components/authentication/Login.js
@@ -4,8 +4,13 @@ export class Login extends Component {
   render() {
     return (
       <div>
-        Login here
+        <form actions="" method="POST">
+          Email: <input type="text" name="email"/><br /><br />
+          Password: <input type="password" name="pwd"/><br /><br />
+          <input type="submit" value="submit" />
+        </form>
       </div>
+      
     )
   }
 }
