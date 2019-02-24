ada6a17ed89fcf38cf8761d20017d818059ec9a5
Jordan Kutz <jkutz@go.olemiss.edu>
2019-02-24 15:37:58 -0600

    added Restaurant component and changed Dashboard from a func to class

--- src/frontend/src/components/authentication/Dashboard.js
+++ src/frontend/src/components/authentication/Dashboard.js
@@ -1,10 +1,28 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import Restaurant from '../customer/Restaurant';
 
-export default function Dashboard() {
-  return (
-    <Fragment>
-        <h1>Welcome</h1>
-        <br />
-    </Fragment>
-  )
+export class Dashboard extends Component{
+  static propTypes = {
+    isAuthenticated: PropTypes.bool
+  }
+
+  render(){
+      return (
+        <Fragment>
+          <h1>Welcome</h1>
+          <br />
+          <Restaurant />
+        </Fragment>
+      )
+  }
+
 }
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.authReducer.isAuthenticated
+});
+
+export default connect(mapStateToProps)(Dashboard);
+
---
+++ src/frontend/src/components/customer/Restaurant.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Restaurant extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Restaurant Name</h3>
+      </div>
+    )
+  }
+}
+
+export default Restaurant;
