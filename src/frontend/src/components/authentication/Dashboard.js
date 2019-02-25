c6f6621a2e65a0e10530c3caadaa732b8bae38c0
Jason Wells <jpwells1@go.olemiss.edu>
2019-02-25 13:57:23 -0600

    Added React Native Elements searchbar component to the frontend section under SearchBar.js. Also, added tags to fragment tags in the Dashboard.js.

--- src/frontend/src/components/authentication/Dashboard.js
+++ src/frontend/src/components/authentication/Dashboard.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from 'react';
+import './SearchBar'
 
 export default function Dashboard() {
   return (
     <Fragment>
         <h1>Welcome</h1>
         <br />
+        <SearchBar />
     </Fragment>
   )
 }
