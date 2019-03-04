b1308c5bbf0d15f185fb0e584138fa98b3b91a4e
Aishwant Ghimire <aghimire@go.olemiss.edu>
2019-03-04 17:53:19 -0600

    Pictures

--- src/frontend/src/components/customer/Restaurant.js
+++ src/frontend/src/components/customer/Restaurant.js
@@ -24,7 +24,7 @@ export class Restaurant extends Component {
 
             <div className="col-md">
               <div className="card" style={cardWidth}>
-                <img className="card-img-top" src="..." alt="Card image cap" />
+                <img className="card-img-top" src="https://firebasestorage.googleapis.com/v0/b/csci387.appspot.com/o/img%2Fevanwise.jpg?alt=media&token=6986eebb-7928-42d6-9d4e-7589990f29b3" alt="Card image cap" />
                 <div className="card-body">
                   <h5 className="card-title">{res.Name}</h5>
                   <p className="card-text">
