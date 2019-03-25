052308d43b82db6b16654e5a2cbd3479bd814e6d
Aishwant Ghimire <aghimire@go.olemiss.edu>
2019-03-25 18:48:37 -0500

    Filter MenuItems working

--- src/frontend/src/components/customer/Restaurant.js
+++ src/frontend/src/components/customer/Restaurant.js
@@ -2,17 +2,20 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { getRestaurant } from '../../actions/getRestaurants';
-
 import { Link } from "react-router-dom";
 
 
 export class Restaurant extends Component {
 
-  static propTypes = {
-    // restaurants: PropTypes.array.isRequired,
-    getRestaurantName: PropTypes.func,
-    getRestaurant: PropTypes.func
-  };
+  // static propTypes = {
+  //   // restaurants: PropTypes.array.isRequired,
+  //   getRestaurantName: PropTypes.func,
+  //   getRestaurant: PropTypes.func
+  // };
+
+  componentDidMount(){
+    this.props.getRestaurant();
+  }
 
   state = {
     query: '',
@@ -23,13 +26,10 @@ export class Restaurant extends Component {
     cityS:false,
     closeS:false,
     cuisineTypeS:false,
+    tagS:false,
     filter:'nameS'
   }
-
-  componentDidMount(){
-    this.props.getRestaurant();
-  }
-
+  
   handleClick= event =>{
     event.preventDefault()
     this.setState({showForm:true})
@@ -44,15 +44,17 @@ export class Restaurant extends Component {
       })
     }
     if(this.state.filter=="nameS"){
-      this.setState({nameS:true,zipcodeS:false,cityS:false,closeS:false,cuisineTypeS:false});
+      this.setState({nameS:true,zipcodeS:false,cityS:false,closeS:false,cuisineTypeS:false,tagS:false});
     }else if(this.state.filter=="zipcodeS"){
-      this.setState({nameS:false,zipcodeS:true,cityS:false,closeS:false,cuisineTypeS:false});
+      this.setState({nameS:false,zipcodeS:true,cityS:false,closeS:false,cuisineTypeS:false,tagS:false});
     }else if(this.state.filter=="cityS"){
-      this.setState({nameS:false,zipcodeS:false,cityS:true,closeS:false,cuisineTypeS:false})
+      this.setState({nameS:false,zipcodeS:false,cityS:true,closeS:false,cuisineTypeS:false,tagS:false})
     }else if(this.state.filter=="closeS"){
-      this.setState({nameS:false,zipcodeS:false,cityS:false,closeS:true,cuisineTypeS:false})
-    }else if(this.state.filter=="cuisineTypeS"){
-      this.setState({nameS:false,zipcodeS:false,cityS:false,closeS:false,cuisineTypeS:true})
+      this.setState({nameS:false,zipcodeS:false,cityS:false,closeS:true,cuisineTypeS:false,tagS:false})
+    }else if(this.state.filter=="cusineTypeS"){
+      this.setState({nameS:false,zipcodeS:false,cityS:false,closeS:false,cuisineTypeS:true,tagS:false})
+    }else if(this.state.filter=="tagS"){
+      this.setState({nameS:false,zipcodeS:false,cityS:false,closeS:false,cuisineTypeS:false,tagS:true})
     }
   }
 
@@ -61,13 +63,13 @@ export class Restaurant extends Component {
   }
 
   render() {
-    const contentKeys = Object.keys(this.props.restaurants);
+    const contentKeys = Object.keys(this.props.restaurants)
     const { filter } = this.state;
-    
+    //console.log((this.props.restaurants))
     return (
       <div className="row">
         <div className="col-md-12">
-          <div className="col-md-6 mt-2" style={divStyle}>
+          <div className="col-md-6 mt-2" style={{marginBottom:"20px"}}>
           <div className="input-group">
           <input
           type="text"
@@ -83,83 +85,82 @@ export class Restaurant extends Component {
               <option value="cityS">City</option>
               <option value="closeS">Close Time</option>
               <option value="cuisineTypeS">Cuisine Type</option>
+              <option value="tagS">Menu Items</option>
             </select>
           </div>
           </div>
 
           
         </div>
-       
+
         {contentKeys.map(t=>
-
-            [this.props.restaurants[t]].map(res =>
-              { if 
-                (this.state.query !== '' &&
-                  (
-                    (this.state.nameS && res.Name.toUpperCase().includes(this.state.query.toUpperCase()))||
-                    (this.state.zipcodeS && res.zipcode.toUpperCase().includes(this.state.query.toUpperCase())) ||
-                    (this.state.cityS && res.City.includes(this.state.query))||
-                    (this.state.closeS && res.Close.includes(this.state.query)) ||
-                    (this.state.cuisineTypeS && res.CuisineType.toUpperCase().includes(this.state.query.toUpperCase()))
-                  )
+        
+          [this.props.restaurants[t]].map(res =>
+           { if 
+              (this.state.query !== '' &&
+                (
+                  (this.state.nameS && res.Name.toUpperCase().includes(this.state.query.toUpperCase()))||
+                  (this.state.zipcodeS && res.zipcode.toUpperCase().includes(this.state.query.toUpperCase())) ||
+                  (this.state.cityS && res.City.includes(this.state.query))||
+                  (this.state.closeS && res.Close.includes(this.state.query)) ||
+                  (this.state.cuisineTypeS && res.CuisineType.toUpperCase().includes(this.state.query.toUpperCase())) ||
+                  (this.state.tagS && res.tags.toUpperCase().includes(this.state.query.toUpperCase()))
                 )
-                {
-                  return (
-                    <div className="col-md-3" key={res.Name}>
-                      <div className="card" style={cardWidth}>
-                        <img className="card-img-top" src="https://firebasestorage.googleapis.com/v0/b/csci387.appspot.com/o/img%2Fevanwise.jpg?alt=media&token=6986eebb-7928-42d6-9d4e-7589990f29b3" alt="Card image cap" />
-                        <div className="card-body">
-                          <h5 className="card-title">{res.Name}</h5>
-                          <div className="card-text">
-                            <h6>{res.Address}</h6>
-                            <h6>{res.City} {res.zipcode}</h6> 
-                            <h6>Type: {res.CuisineType}</h6>
-                            <h6>Open: {res.Open}</h6>
-                            <h6>Close: {res.Close}</h6>
-                          </div>
-                          <div className="row">
-                            <Link to={`/editmenu/${res.Name}?id=${t}`} name={res.Name} className="btn btn-primary">Menu</Link>
-                          </div>
-                        </div>
+              )
+              {
+             
+              return (
+              <div className="col-md-3" key={res.Name}>
+                <div className="card" style={cardWidth}>
+                  <img className="card-img-top" src="https://firebasestorage.googleapis.com/v0/b/csci387.appspot.com/o/img%2Fevanwise.jpg?alt=media&token=6986eebb-7928-42d6-9d4e-7589990f29b3" alt="Card image cap" />
+                  <div className="card-body">
+                    <h5 className="card-title">{res.Name}</h5>
+                    <div className="card-text">
+                      <h6>{res.Address}</h6>
+                      <h6>{res.City} {res.zipcode}</h6> 
+                      <h6>Type: {res.CuisineType}</h6>
+                      <h6>Open: {res.Open}</h6>
+                      <h6>Close: {res.Close}</h6>
                     </div>
+                    <div className="row">
+                      <Link to={`/editmenu/${res.Name}?id=${t}`} name={res.Name} className="btn btn-primary">Menu</Link>
                     </div>
-                  )
-                  }else{
-                    if(this.state.query){
-                      return('')
-                    }else{
-                      return (
-                        <div className="col-md-3" key={res.Name}>
-                          <div className="card" style={cardWidth}>
-                            <img className="card-img-top" src="https://firebasestorage.googleapis.com/v0/b/csci387.appspot.com/o/img%2Fevanwise.jpg?alt=media&token=6986eebb-7928-42d6-9d4e-7589990f29b3" alt="Card image cap" />
-                            <div className="card-body">
-                              <h5 className="card-title">{res.Name}</h5>
-                              <div className="card-text">
-                                <h6>{res.Address}</h6>
-                                <h6>{res.City} {res.zipcode}</h6>
-                                <h6>Type: {res.CuisineType}</h6> 
-                                <h6>Open: {res.Open}</h6>
-                                <h6>Close: {res.Close}</h6>
-                              </div>
-                              <div className="row">
-                                <Link to={`/editmenu/${res.Name}?id=${t}`} name={res.Name} className="btn btn-primary">Menu</Link>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      )
-                    }
-                  }
-              }
+                  </div>
+              </div>
+              </div>
             )
-        )}
-        </div>
+            }else{
+              if(this.state.query){
+                return('')
+              }else{
+                return (
+                  <div className="col-md-3" key={res.Name}>
+                    <div className="card" style={cardWidth}>
+                    <img className="card-img-top" src="https://firebasestorage.googleapis.com/v0/b/csci387.appspot.com/o/img%2Fevanwise.jpg?alt=media&token=6986eebb-7928-42d6-9d4e-7589990f29b3" alt="Card image cap" />
+                    <div className="card-body">
+                      <h5 className="card-title">{res.Name}</h5>
+                      <div className="card-text">
+                        <h6>{res.Address}</h6>
+                        <h6>{res.City} {res.zipcode}</h6>
+                        <h6>Type: {res.CuisineType}</h6> 
+                        <h6>Open: {res.Open}</h6>
+                        <h6>Close: {res.Close}</h6>
+                      </div>
+                      <div className="row">
+                        <Link to={`/editmenu/${res.Name}?id=${t}`} name={res.Name} className="btn btn-primary">Menu</Link>
+                      </div>
+                  </div>
+                </div>
+              </div>
+            )
+          }
+        }
+      }
       )
+      )}
+      </div>
+    )
   }
-}
-
-const divStyle = {
-  marginBottom:"20px",
 }
 
 const cardWidth = {
