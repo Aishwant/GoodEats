2db647b298d1d317e4354428726ee58c4d34e977
Jordan Kutz <jkutz@go.olemiss.edu>
2019-03-29 13:27:46 -0500

    Fixed customer  menu view loading owner menu view

--- src/frontend/src/components/customer/Restaurant.js
+++ src/frontend/src/components/customer/Restaurant.js
@@ -118,7 +118,7 @@ export class Restaurant extends Component {
                             <h6>Close: {res.Close}</h6>
                           </div>
                           <div className="row">
-                            <Link to={`/editmenu/${res.Name}?id=${t}`} name={res.Name} className="btn btn-primary">Menu</Link>
+                            <Link to={`/menu/${res.Name}?id=${t}`} name={res.Name} className="btn btn-primary">Menu</Link>
                           </div>
                         </div>
                     </div>
@@ -142,7 +142,7 @@ export class Restaurant extends Component {
                                 <h6>Close: {res.Close}</h6>
                               </div>
                               <div className="row">
-                                <Link to={`/editmenu/${res.Name}?id=${t}`} name={res.Name} className="btn btn-primary">Menu</Link>
+                                <Link to={`/menu/${res.Name}?id=${t}`} name={res.Name} className="btn btn-primary">Menu</Link>
                               </div>
                             </div>
                           </div>
