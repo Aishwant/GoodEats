ca2bea58e6bcd26f0d26b88e24242cf7a6fd80dd
Jordan Kutz <jkutz@go.olemiss.edu>
2019-03-30 00:29:14 -0500

    Restyled quantity buttons

--- src/frontend/src/components/customer/Quantity.js
+++ src/frontend/src/components/customer/Quantity.js
@@ -17,15 +17,17 @@ export class Quantity extends Component {
   render() {
     return (
         <div>
-        <button className="btn btn-success btn-sm mt-1"  onClick={this.props.addToCartChild.bind(this, this.props.itemID, this.props.itemData, this.state.Quantity)}>Add</button>
+        <button className="btn btn-success btn-sm btn-block mt-1"  onClick={this.props.addToCartChild.bind(this, this.props.itemID, this.props.itemData, this.state.Quantity)}>Add</button>
         <div className="row">
-        <div className="input-group">
+        <div className="input-group mt-1">
           <span className="input-group-btn">
-              <button type="button" className="btn btn-default btn-number" onClick={this.decrementQuantity.bind(this)}>
+              <button type="button" className="btn btn-default btn-number ml-2" onClick={this.decrementQuantity.bind(this)}>
                   -
               </button>
           </span>
+          <div className="mt-2">
           {this.state.Quantity}
+          </div>
           <span className="input-group-btn">
               <button type="button" className="btn btn-default btn-number" onClick={this.incrementQuantity.bind(this)}>
               +
