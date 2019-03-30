1d664e0e162630fdc810c2565fb697683f42eafb
Jordan Kutz <jkutz@go.olemiss.edu>
2019-03-30 18:11:37 -0500

    Added password rules

--- src/frontend/src/actions/addNewUser.js
+++ src/frontend/src/actions/addNewUser.js
@@ -1,20 +1,4 @@
-import React, { Component } from 'react'
 import axios from "axios";
-import { Redirect } from "react-router-dom";
-
-//Authenticate
-
-import {
-  USER_LOADED,
-  USER_LOADING,
-  AUTH_ERROR,
-  GET_ERRORS,
-  LOGIN_FAIL,
-  LOGIN_SUCCESS,
-  LOGOUT_SUCCESS,
-  REGISTER_SUCCESS,
-  CUSTOMER_ADDED
-} from './types';
 
 export const addUser = (info) => dispatch => {
   const uuidv4 = require('uuid/v4');
@@ -28,22 +12,7 @@ export const addUser = (info) => dispatch => {
     axios
       .post("/api/database/newuser", toData)
       .then(res => {
-        console.log(res.data)
-        // if (res.data.status=="success"){
-        //   dispatch({
-        //     type: CUSTOMER_ADDED,
-        //     payload: res.data
-        //   });
-        // }else{
-        //   localStorage.removeItem('uID');
-        //   dispatch({
-        //     type:LOGIN_FAIL
-        //   });
-        //   dispatch({
-        //     type: GET_ERRORS,
-        //     payload: res.data
-        //   });
-        // }
+        
       })
       .catch(err => {});
   };
