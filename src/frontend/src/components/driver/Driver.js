113ff8c4a383cbe8a1658dfefc817de01061350e
Madhav Prasad Koirala <mkoirala@go.olemiss.edu>
2019-04-06 15:58:51 -0500

    View Order

--- src/frontend/src/components/driver/Driver.js
+++ src/frontend/src/components/driver/Driver.js
@@ -27,26 +27,26 @@ export default class Driver extends Component {
 
        <ul class="nav nav-tabs nav-justified tabs indigo" id="myTabJust" role="tablist">
         <li class="nav-item">
-          <a class="nav-link active" id="home-tab-just" data-toggle="tab" href="#home-just" role="tab" aria-controls="home-just"
-            aria-selected="true"><i class="fas fa-shopping-bag"></i><br/>New Order</a>
+          <a class="nav-link active" id="NewOrder-tab-just" data-toggle="tab" href="#home-just" role="tab" aria-controls="home-just"
+            aria-selected="true"><i class="fas fa-shopping-bag fa-3x"></i><br/>New Order</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" id="profile-tab-just" data-toggle="tab" href="#profile-just" role="tab" aria-controls="profile-just"
-            aria-selected="false"><i class="fas fa-truck"></i><br/>Pending Order</a>
+          <a class="nav-link" id="OutForDelivey-tab-just" data-toggle="tab" href="#profile-just" role="tab" aria-controls="profile-just"
+            aria-selected="false"><i class="fas fa-truck fa-3x"></i><br/>Out For Delivery</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" id="contact-tab-just" data-toggle="tab" href="#contact-just" role="tab" aria-controls="contact-just"
-            aria-selected="false"><i class="fas fa-history"></i><br/>Order History</a>
+          <a class="nav-link" id="OrderHistory-tab-just" data-toggle="tab" href="#contact-just" role="tab" aria-controls="contact-just"
+            aria-selected="false"><i class="fas fa-history fa-3x"></i><br/>Order History</a>
         </li>
       </ul>
       <div class="tab-content card pt-5" id="myTabContentJust">
-        <div class="tab-pane fade show active" id="home-just" role="tabpanel" aria-labelledby="home-tab-just">
+        <div class="tab-pane fade show active" id="NewOrder-tab-just" role="tabpanel" aria-labelledby="home-tab-just">
         <DriverPlacedOrder/>
         </div>
-        <div class="tab-pane fade" id="profile-just" role="tabpanel" aria-labelledby="profile-tab-just">
+        <div class="tab-pane fade" id="OutForDelivey-tab-just" role="tabpanel" aria-labelledby="profile-tab-just">
         <DriverPlacedOrder/>
         </div>
-        <div class="tab-pane fade" id="contact-just" role="tabpanel" aria-labelledby="contact-tab-just">
+        <div class="tab-pane fade" id="OrderHistory-tab-just" role="tabpanel" aria-labelledby="contact-tab-just">
         <DriverPlacedOrder/>
         </div>
       </div>
--- src/frontend/src/components/driver/DriverCard.js
+++ src/frontend/src/components/driver/DriverCard.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
-import Button from 'react-bootstrap/Button'
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 
 
@@ -15,9 +16,25 @@ import Button from 'react-bootstrap/Button'
 
 export default class DriverCard extends Component{
 
-    constructor(props) {
-        super(props);
-    } 
+  
+    constructor(props, context) {
+        super(props, context);
+    
+        this.handleShow = this.handleShow.bind(this);
+        this.handleClose = this.handleClose.bind(this);
+    
+        this.state = {
+          show: false,
+        };
+      }
+    
+      handleClose() {
+        this.setState({ show: false });
+      }
+    
+      handleShow() {
+        this.setState({ show: true });
+      }
 
     render(){
         
@@ -31,9 +48,40 @@ export default class DriverCard extends Component{
                         <h4 class="card-title">{CID}</h4>
                         <p class="card-text"><h5>{RID}</h5></p>
                         {/* <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> */}
-                        <Button variant="outline-success"onClick={() => {this.props.removeOrder(OrderID);this.props.showOrder(OrderID)}}style={{width:'33%'}}><i class="fas fa-check-circle"></i><br/>Accept</Button>
-                        <Button variant="outline-primary"style={{width:'33%'}}><i class="fas fa-book-open"></i><br/>View</Button>
-                        <Button variant="outline-danger"style={{width:'33%'}}><i class="far fa-times-circle"></i><br/>Decline</Button>
+                        <Button variant="outline-success"onClick={() => {this.props.removeOrder(OrderID);this.props.showOrder(OrderID)}}style={{width:'33%'}}><i class="fas fa-check-circle fa-lg"></i><br/>Accept</Button>
+                        <Button variant="outline-primary" onClick={this.handleShow} style={{width:'33%'}}><i class="fas fa-book-open fa-lg" fa-lg></i><br/>View</Button>
+
+                        <Modal size="lg" show={this.state.show} onHide={this.handleClose}  dialogClassName="modal-90w"
+          >
+                            <Modal.Header closeButton>
+                             <Modal.Title ><h3 style={{margin:0}}>OrderID</h3></Modal.Title>
+                            </Modal.Header>
+                            <Modal.Body>
+                                <div class="row">
+                                <div class="col">
+                                <h4>Pick From</h4>
+                                <h3>Restaurant Test Name</h3>
+                                <h5>Restaurant Street Address</h5>
+                                <h6>City, Zip Code</h6>
+                                </div>
+
+                                <div class="col">
+                                <h4>Deliver To</h4>
+                                <h3>Customer Test Name</h3>
+                                <h5>Customer Street Address</h5>
+                                <h6>City, Zip Code</h6>
+                                </div>
+                                
+                                </div>
+                            </Modal.Body>
+                            <Modal.Footer>
+                                <Button variant="outline-danger" onClick={this.handleClose} style={{justifyContent:'center'}}>
+                                Close
+                                </Button>
+                                
+                            </Modal.Footer>
+                            </Modal>
+                        <Button variant="outline-danger"style={{width:'33%'}}><i class="far fa-times-circle fa-lg" fa-lg></i><br/>Decline</Button>
                      
                     </div>
                 </div>
