0541d7f97008468be701d11a56701f3a57e1563d
Aishwant Ghimire <aghimire@go.olemiss.edu>
2019-04-28 14:34:06 -0500

    Email Api created

---
+++ src/goodeats9/email.py
@@ -0,0 +1,24 @@
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+from rest_framework import viewsets, permissions, generics
+from rest_framework.response import Response
+
+class sendEmailAPI(generics.GenericAPIView):
+
+    permission_classes=[
+        permissions.AllowAny
+    ]
+
+    def post(self, request):
+        message = Mail(
+            from_email='admin@goodeats9.com',
+            to_emails=request.data['email'],
+            subject='Your Order Status',
+            html_content='<strong> Your order is '+request.data['status']+'</strong>')
+        try:
+            sg = SendGridAPIClient('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')
+            response = sg.send(message)
+            return Response()
+        except Exception as e:
+            print(e.message)
--- src/goodeats9/urls.py
+++ src/goodeats9/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
+from .email import sendEmailAPI
 
 urlpatterns = [
     path('',include('frontend.urls')),
     path('',include('authentication.urls')),
-    path('',include('database.urls'))
+    path('',include('database.urls')),
+    path('api/sendemail', sendEmailAPI.as_view())
 ]
