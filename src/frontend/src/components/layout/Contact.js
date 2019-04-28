ed17144a92e6c23e689256a79709999086edc183
Andrew Jelson <arjelson@go.olemiss.edu>
2019-04-28 16:34:36 -0500

    style

--- src/frontend/src/components/layout/Contact.js
+++ src/frontend/src/components/layout/Contact.js
@@ -12,7 +12,18 @@ export class Contact extends Component{
            <div class="col-md-4 team-box">
            <div class="team-img thumbnail">
                     <div class="team-content">    
-                        <h3>David Smith</h3>
+                        <h3>Jordan Kutz</h3>
+                        <div class="border-team"></div>
+                        <p>Hi. I am working on this project</p>
+                        <div class="social-icons"> 
+                            <a href="https://www.facebook.com/"><i id="social-fb" class="fa fa-facebook-square fa-3x social"></i></a>
+                            <a href="https://twitter.com/"><i id="social-tw" class="fa fa-twitter-square fa-3x social"></i></a>
+                            <a href="https://plus.google.com/"><i id="social-gp" class="fa fa-google-plus-square fa-3x social"></i></a>
+                            <a href="mailto:bootsnipp@gmail.com"><i id="social-em" class="fa fa-envelope-square fa-3x social"></i></a>
+                        </div>
+                    </div>
+                    <div class="team-content">    
+                        <h3>Aishwant Ghimire</h3>
                         <div class="border-team"></div>
                         <p>Hi. I am working on this project</p>
                         <div class="social-icons"> 
