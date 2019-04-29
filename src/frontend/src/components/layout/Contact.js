4a0ab619f3d7e5637a4c362c75d2139f9037fd4a
Andrew Jelson <arjelson@go.olemiss.edu>
2019-04-28 20:18:27 -0500

    finished up contact page

--- src/frontend/src/components/layout/Contact.js
+++ src/frontend/src/components/layout/Contact.js
@@ -14,57 +14,32 @@ export class Contact extends Component{
                     <div className="team-content" align="center">    
                         <h3>Jordan Kutz</h3>
                         <div className="border-team" align="center"></div>
-                        <p>Hi. I am working on this project</p>
-                        <div className="social-icons"> 
-                            <a href="https://www.facebook.com/"><i id="social-fb" class="fa fa-facebook-square fa-3x social"></i></a>
-                            <a href="https://twitter.com/"><i id="social-tw" class="fa fa-twitter-square fa-3x social"></i></a>
-                            <a href="https://plus.google.com/"><i id="social-gp" class="fa fa-google-plus-square fa-3x social"></i></a>
-                            <a href="mailto:bootsnipp@gmail.com"><i id="social-em" class="fa fa-envelope-square fa-3x social"></i></a>
-                        </div>
+                        <p>Hi. I am the project manager for our group.</p>
+                        <br/>
                     </div>
                     <div className="team-content" align="center">    
                         <h3>Aishwant Ghimire</h3>
-                        <div className="border-team"></div>
-                        <p>Hi. I am working on this project</p>
-                        <div className="social-icons"> 
-                            <a href="https://www.facebook.com/"><i id="social-fb" class="fa fa-facebook-square fa-3x social"></i></a>
-                            <a href="https://twitter.com/"><i id="social-tw" class="fa fa-twitter-square fa-3x social"></i></a>
-                            <a href="https://plus.google.com/"><i id="social-gp" class="fa fa-google-plus-square fa-3x social"></i></a>
-                            <a href="mailto:bootsnipp@gmail.com"><i id="social-em" class="fa fa-envelope-square fa-3x social"></i></a>
-                        </div>
+                        <div className="border-team" align="center"></div>
+                        <p>Hi. I am the database man for our group.</p>
+                        <br/>
                     </div>
-                    <div className="team-content" align="center">    
+                    <div className="team-content" align="center">     
                         <h3>Madhav Koirala</h3>
-                        <div className="border-team"></div>
-                        <p>Hi. I am working on this project</p>
-                        <div className="social-icons"> 
-                            <a href="https://www.facebook.com/"><i id="social-fb" class="fa fa-facebook-square fa-3x social"></i></a>
-                            <a href="https://twitter.com/"><i id="social-tw" class="fa fa-twitter-square fa-3x social"></i></a>
-                            <a href="https://plus.google.com/"><i id="social-gp" class="fa fa-google-plus-square fa-3x social"></i></a>
-                            <a href="mailto:bootsnipp@gmail.com"><i id="social-em" class="fa fa-envelope-square fa-3x social"></i></a>
-                        </div>
+                        <div className="border-team" align="center"></div>
+                        <p>Hi. I have worked on the functionality of the project</p>
+                        <br/>
                     </div>
                     <div className="team-content" align="center">    
                         <h3>Jason Wells</h3>
-                        <div className="border-team"></div>
-                        <p>Hi. I am working on this project</p>
-                        <div className="social-icons"> 
-                            <a href="https://www.facebook.com/"><i id="social-fb" class="fa fa-facebook-square fa-3x social"></i></a>
-                            <a href="https://twitter.com/"><i id="social-tw" class="fa fa-twitter-square fa-3x social"></i></a>
-                            <a href="https://plus.google.com/"><i id="social-gp" class="fa fa-google-plus-square fa-3x social"></i></a>
-                            <a href="mailto:bootsnipp@gmail.com"><i id="social-em" class="fa fa-envelope-square fa-3x social"></i></a>
-                        </div>
+                        <div className="border-team" align="center"></div>
+                        <p>Hi. I am worked on implementing some unique components of this project</p>
+                        <br/>
                     </div>
                     <div className="team-content" align="center">    
                         <h3>Andrew Jelson</h3>
-                        <div className="border-team"></div>
-                        <p>Hi. I am working on this project</p>
-                        <div className="social-icons"> 
-                            <a href="https://www.facebook.com/"><i id="social-fb" class="fa fa-facebook-square fa-3x social"></i></a>
-                            <a href="https://twitter.com/"><i id="social-tw" class="fa fa-twitter-square fa-3x social"></i></a>
-                            <a href="https://plus.google.com/"><i id="social-gp" class="fa fa-google-plus-square fa-3x social"></i></a>
-                            <a href="mailto:bootsnipp@gmail.com"><i id="social-em" class="fa fa-envelope-square fa-3x social"></i></a>
-                        </div>
+                        <div className="border-team" align="center"></div>
+                        <p>Hi. I am the server guy for our project</p>
+                        <br/>
                     </div>
                 </div> 
             </div>
