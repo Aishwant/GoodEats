0cc28ba3ee7b6e7290363fe1fa7625675a6105b2
Andrew Jelson <arjelson@go.olemiss.edu>
2019-04-28 19:02:12 -0500

    Finish up contact page

--- src/frontend/src/components/layout/Contact.js
+++ src/frontend/src/components/layout/Contact.js
@@ -9,46 +9,57 @@ export class Contact extends Component{
         return (
            <div>
            <h1 align="center">Contact Us</h1>
-           <div class="col-md-4 team-box">
-           <div class="team-img thumbnail">
-                    <div class="team-content" align="center">    
+           <div className="col-md-4 team-box">
+           <div className="team-img thumbnail">
+                    <div className="team-content" align="center">    
                         <h3>Jordan Kutz</h3>
-                        <div class="border-team" align="center"></div>
+                        <div className="border-team" align="center"></div>
                         <p>Hi. I am working on this project</p>
-                        <div class="social-icons"> 
+                        <div className="social-icons"> 
                             <a href="https://www.facebook.com/"><i id="social-fb" class="fa fa-facebook-square fa-3x social"></i></a>
                             <a href="https://twitter.com/"><i id="social-tw" class="fa fa-twitter-square fa-3x social"></i></a>
                             <a href="https://plus.google.com/"><i id="social-gp" class="fa fa-google-plus-square fa-3x social"></i></a>
                             <a href="mailto:bootsnipp@gmail.com"><i id="social-em" class="fa fa-envelope-square fa-3x social"></i></a>
                         </div>
                     </div>
-                    <div class="team-content" align="center">    
+                    <div className="team-content" align="center">    
                         <h3>Aishwant Ghimire</h3>
-                        <div class="border-team"></div>
+                        <div className="border-team"></div>
                         <p>Hi. I am working on this project</p>
-                        <div class="social-icons"> 
+                        <div className="social-icons"> 
                             <a href="https://www.facebook.com/"><i id="social-fb" class="fa fa-facebook-square fa-3x social"></i></a>
                             <a href="https://twitter.com/"><i id="social-tw" class="fa fa-twitter-square fa-3x social"></i></a>
                             <a href="https://plus.google.com/"><i id="social-gp" class="fa fa-google-plus-square fa-3x social"></i></a>
                             <a href="mailto:bootsnipp@gmail.com"><i id="social-em" class="fa fa-envelope-square fa-3x social"></i></a>
                         </div>
                     </div>
-                    <div class="team-content" align="center">    
+                    <div className="team-content" align="center">    
                         <h3>Madhav Koirala</h3>
-                        <div class="border-team"></div>
+                        <div className="border-team"></div>
                         <p>Hi. I am working on this project</p>
-                        <div class="social-icons"> 
+                        <div className="social-icons"> 
                             <a href="https://www.facebook.com/"><i id="social-fb" class="fa fa-facebook-square fa-3x social"></i></a>
                             <a href="https://twitter.com/"><i id="social-tw" class="fa fa-twitter-square fa-3x social"></i></a>
                             <a href="https://plus.google.com/"><i id="social-gp" class="fa fa-google-plus-square fa-3x social"></i></a>
                             <a href="mailto:bootsnipp@gmail.com"><i id="social-em" class="fa fa-envelope-square fa-3x social"></i></a>
                         </div>
                     </div>
-                    <div class="team-content" align="center">    
+                    <div className="team-content" align="center">    
                         <h3>Jason Wells</h3>
-                        <div class="border-team"></div>
+                        <div className="border-team"></div>
                         <p>Hi. I am working on this project</p>
-                        <div class="social-icons"> 
+                        <div className="social-icons"> 
+                            <a href="https://www.facebook.com/"><i id="social-fb" class="fa fa-facebook-square fa-3x social"></i></a>
+                            <a href="https://twitter.com/"><i id="social-tw" class="fa fa-twitter-square fa-3x social"></i></a>
+                            <a href="https://plus.google.com/"><i id="social-gp" class="fa fa-google-plus-square fa-3x social"></i></a>
+                            <a href="mailto:bootsnipp@gmail.com"><i id="social-em" class="fa fa-envelope-square fa-3x social"></i></a>
+                        </div>
+                    </div>
+                    <div className="team-content" align="center">    
+                        <h3>Andrew Jelson</h3>
+                        <div className="border-team"></div>
+                        <p>Hi. I am working on this project</p>
+                        <div className="social-icons"> 
                             <a href="https://www.facebook.com/"><i id="social-fb" class="fa fa-facebook-square fa-3x social"></i></a>
                             <a href="https://twitter.com/"><i id="social-tw" class="fa fa-twitter-square fa-3x social"></i></a>
                             <a href="https://plus.google.com/"><i id="social-gp" class="fa fa-google-plus-square fa-3x social"></i></a>
